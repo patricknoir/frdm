273be50279b14ca49a0d19c26adc41198a46f0f8
Patrick Di Loreto <diloreto.p@gmail.com>
2016-06-11 17:41:19 +0200

    adding utility functions get/set

--- src/main/scala/org/patricknoir/frdm/impl/RepositoryImpl.scala
+++ src/main/scala/org/patricknoir/frdm/impl/RepositoryImpl.scala
@@ -20,17 +20,17 @@ case class InMemoryRepository[Id, Entity](keyExtractor: Reader[Entity, Id], enti
     (this, Xor.fromTry(Try(r)).leftMap(ex => List(ex.getMessage)))
   }
 
-  def set[A](s: Repository[Id, Entity])(r: => A): Response[A] = State { _ =>
-    (s, run(r))
+
+  def modify[A](s: Map[Id, Entity])(r: => A): Response[A] =  State { _ =>
+    (this.copy(entities = s), run(r))
   }
 
   def query(id: Id): Response[Entity] = get { entities(id) }
 
   def store(entity: Entity): Response[Id] = store(keyExtractor(entity), entity)
 
-  private def store(key: Id, value: Entity): Response[Id] = State { _ =>
-    (this.copy(entities = entities + (key -> value)), run(key))
-  }
+  private def store(key: Id, value: Entity): Response[Id] = modify(entities + (key->value))(key)
+
 }
 
 object BetInMemoryRepository extends InMemoryRepository[BetReceipt, Bet](Reader[Bet, BetReceipt](bet => BetReceipt(bet.id)))
